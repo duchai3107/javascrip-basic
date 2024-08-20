41d55712b33e8404ae905415dcb3175d42b229a6
Nguyễn Đức Hải <148833400+duchai3107@users.noreply.github.com>
2024-08-20 15:37:46 +0700

    vidoe6

---
+++ thuchanh/video4.js
@@ -0,0 +1,8 @@
+let a = "i'm a hero" + `tiki ttaka`;
+console.log(a)
+let b = "telegram \
+kkkkkkk";
+console.log(b);
+console.log(a.length);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
